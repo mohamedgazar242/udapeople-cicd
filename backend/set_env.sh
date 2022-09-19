919f313bdba6b9b85a0022531fdedd966a364d97
unknown <mohamedgazar242@gmail.com>
2022-09-19 04:03:56 +0200

    dploy infra

---
+++ backend/set_env.sh
@@ -0,0 +1,8 @@
+export TYPEORM_HOST=localhost
+export TYPEORM_PORT=5532
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=password
+export TYPEORM_DATABASE=glee
+export TYPEORM_MIGRATIONS_DIR=./migrations
+export TYPEORM_ENTITIES=./modules/domain/**/*.entity{.ts,.js}
+export TYPEORM_MIGRATIONS=./migrations/*.ts
